39ef96ced7d2a27862d6e245c9c3d75dd180282d
Aslan Mislishaev <muroshkamoon@gmail.com>
2025-07-16 17:30:51 +0300

    16.07.2025 - 17:30

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  build: {
+    minify: 'terser',           // уменьшает размер
+    sourcemap: false,           // отключает карты — они не нужны на проде
+    chunkSizeWarningLimit: 500  // предупреждение, если файл >500кб
+  }
+})
